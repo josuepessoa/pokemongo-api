af932887191658a41804982b9da4d7d273a8009c
Josue Matheus Pessoa <josue.matheus12@gmail.com>
2022-01-05 01:47:36 -0300

    CORS

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8877;
 const arq = require("./pokemons.json");
+const cors = require('cors');
+
+
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100',
+  'http://localhost:4200'
+];
 let find = false
 
-app.get("/status", (req, res) => {
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
+  };
+
+app.options('*', cors(corsOptions));
+
+app.get("/status", cors(corsOptions), (req, res) => {
     res.json({
         msg: "ok",
     });
 });
 
-app.get("/pokemons", (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
     const id = req.query.id;
 
     if (typeof id === "undefined") {
